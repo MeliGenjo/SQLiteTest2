9ad78701742e521f0b077f6b1a72196a40456270
Meli <genjo.melina@gmail.com>
2016-07-03 13:03:22 -0300

    ABM con SQlite

---
+++ app/src/main/java/com/example/mel/sqlitetest2/AdminSQLiteOpenHelper.java
@@ -0,0 +1,27 @@
+package com.example.mel.sqlitetest2;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteDatabase.CursorFactory;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class AdminSQLiteOpenHelper extends SQLiteOpenHelper {
+
+    //llamamos al constructor
+    public AdminSQLiteOpenHelper(Context context, String nombre, CursorFactory factory, int version) {
+        super(context, nombre, factory, version);
+    }
+
+    //creamos la tabla
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("create table votantes(dni integer primary key, nombre text, colegio text, nromesa integer)");
+    }
+
+    //borrar la tabla y crear la nueva tabla
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int versionAnte, int versionNue) {
+        db.execSQL("drop table if exists votantes");
+        db.execSQL("create table votantes(dni integer primary key, nombre text, colegio text, nromesa integer)");
+    }
+}
